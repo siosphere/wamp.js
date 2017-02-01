ed0a365d7cf8a91043d09aa6eb7c32ec8410cc13
Michael Kramer <mkramer@coursehero.com>
2017-01-31 21:56:31 -0800

    Fix WAMP_FEATURES being empty

--- js/lib/session.js
+++ js/lib/session.js
@@ -9,7 +9,44 @@ Date.now = Date.now || function () {
 
 // WAMP "Advanced Profile" support in AutobahnJS per role
 // ignore now.
-const WAMP_FEATURES = {};
+const WAMP_FEATURES = {
+   caller: {
+      features: {
+         caller_identification: true,
+         //call_timeout: true,
+         //call_canceling: true,
+         progressive_call_results: true
+      }
+   },
+   callee: {
+      features: {
+         caller_identification: true,
+         //call_trustlevels: true,
+         pattern_based_registration: true,
+         shared_registration: true,
+         //call_timeout: true,
+         //call_canceling: true,
+         progressive_call_results: true,
+         registration_revocation: true
+      }
+   },
+   publisher: {
+      features: {
+         publisher_identification: true,
+         subscriber_blackwhite_listing: true,
+         publisher_exclusion: true
+      }
+   },
+   subscriber: {
+      features: {
+         publisher_identification: true,
+         //publication_trustlevels: true,
+         pattern_based_subscription: true,
+         subscription_revocation: true
+         //event_history: true,
+      }
+   }
+}
 
 
 // replace when/function/call
